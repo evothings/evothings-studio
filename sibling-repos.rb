c40e687548a49213c2cde67376a5e8f7f9bf7312
Mikael Kindborg <mikael.kindborg@gmail.com>
2016-02-03 16:56:36 +0100

    Commented out unused methods, removed depenent repos not used, updated path to evothings-examples/examples to 'evothings-examples/generated/examples'.

--- sibling-repos.rb
+++ sibling-repos.rb
@@ -2,10 +2,6 @@ SiblingRepo = Struct.new(:name, :url)
 
 def siblingRepos
 	[
-		SiblingRepo.new('evothings-client'),
-		SiblingRepo.new('evothings-doc'),
-		SiblingRepo.new('evothings-examples'),
-		SiblingRepo.new('cordova-plugin-ibeacon', 'https://github.com/petermetz/cordova-plugin-ibeacon'),
-		SiblingRepo.new('cordova-ble'),
+		SiblingRepo.new('evothings-examples')
 	]
 end
