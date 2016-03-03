1fcb0311432f92d7eb63dc48f928441804fc1c24
Peter Svensson <psvenson@gmail.com>
2016-03-03 09:11:51 +0100

    made change to data format from isntrumentation to include data type, in preparation of watch data (non plot)

--- injectables/bluetooth-instrumentation.js
+++ injectables/bluetooth-instrumentation.js
@@ -1,16 +1,6 @@
 
 hyper.log ('bluetooth intrumentation provider loading....')
 
-function generateUUID() {
-    var d = new Date().getTime();
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = (d + Math.random()*16)%16 | 0;
-        d = Math.floor(d/16);
-        return (c=='x' ? r : (r&3|8)).toString(16);
-    });
-    return uuid;
-}
-
 var me = window.evo.bluetooth =
 {
     bluetoothServices: {
