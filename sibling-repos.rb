e9293e2280783cb8259b0bb2973a82bd804eff43
Mikael Kindborg <mikael.kindborg@gmail.com>
2016-03-08 20:55:57 +0100

    Added evothings-libraries as a sibling repo.

--- sibling-repos.rb
+++ sibling-repos.rb
@@ -2,6 +2,7 @@ SiblingRepo = Struct.new(:name, :url)
 
 def siblingRepos
 	[
+		SiblingRepo.new('evothings-libraries'),
 		SiblingRepo.new('evothings-examples'),
 		SiblingRepo.new('evothings-viewer'),
 	]
