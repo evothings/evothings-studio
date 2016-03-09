c228f61b9072060ba7f0e4b5b448f129f3514869
Mikael Kindborg <mikael.kindborg@gmail.com>
2016-03-09 18:14:00 +0100

    Increased default setting of folder depth to monitor from 3 to 5.

--- hyper/settings/settings.js
+++ hyper/settings/settings.js
@@ -75,9 +75,9 @@ function defineSettingFuns(name, defaultValue)
  * project that is running is reloaded on all connected
  * devices/browsers when a file is edited and saved.
  * To scan files in the top-level directory only, set
- * this value to 1. Default value is 3.
+ * this value to 1. Default value is 5.
  */
-systemSetting('NumberOfDirecoryLevelsToTraverse', 3)
+systemSetting('NumberOfDirecoryLevelsToTraverse', 5)
 
 /**
  * If this setting is true, the Hyper server will
