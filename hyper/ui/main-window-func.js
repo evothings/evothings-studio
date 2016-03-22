28a4ef73b93d3bb24ab2a624e1d1c5a1f95db4e5
Mikael Kindborg <mikael.kindborg@gmail.com>
2016-03-22 11:17:18 +0100

    Added error handling for dropped files and invalid (non-existing) app entries.

--- hyper/ui/main-window-func.js
+++ hyper/ui/main-window-func.js
@@ -284,7 +284,7 @@ exports.defineUIFunctions = function(hyper)
 			}
 			else
 			{
-				alert('Only evothings.json or HTML files (extension .html or .htm) can be used')
+				window.alert('Not a valid evothings.json file or HTML file (extension .html or .htm)')
 				break;
 			}
 		}
@@ -294,9 +294,40 @@ exports.defineUIFunctions = function(hyper)
 
     function pathIsValidAppPath(path)
     {
-        return FILEUTIL.fileIsHTML(path) ||
-            FILEUTIL.fileIsEvothingsSettings(path) ||
-            FILEUTIL.fileIsDirectory(path)
+        // Is it an existing HTML file?
+		if (FILEUTIL.fileIsHTML(path) && FILEUTIL.statSync(path))
+		{
+			return true
+		}
+
+		// Directory containing evothings.json file.
+		var dirPath = null
+
+		// If path points to evothings.json file, get the directory
+		if (FILEUTIL.fileIsEvothingsSettings(path))
+		{
+			dirPath = PATH.dirname(path)
+		}
+		else if (FILEUTIL.fileIsDirectory(path))
+		{
+			// Dropped file is a directory.
+			dirPath = path
+		}
+
+		// Must have directory to continue.
+		if (!dirPath)
+		{
+			return false
+		}
+
+        // Does the directory have an evothings.json file pointing to existing index file?
+        var indexPath = APP_SETTINGS.getIndexFileFullPath(dirPath)
+		if (FILEUTIL.statSync(indexPath))
+		{
+			return true
+		}
+
+		return false
     }
 
 	/**
@@ -341,6 +372,16 @@ exports.defineUIFunctions = function(hyper)
 			html += '<div class="app-icon" style="background-image: url(\'images/app-icon.png\');"></div>'
 		}
 
+		// Get name of app, uses title tag as first choise.
+		// Returns null if HTML file not found.
+		var appName = hyper.UI.getProjectNameFromFile(appPath)
+		var appHasValidHTMLFile = !!appName
+		if (!appHasValidHTMLFile)
+		{
+			// If app name was not found, index.html does not exist.
+			appName = 'Warning: HTML file does not exist'
+		}
+
 		if (docURL && options.docButton)
 		{
 			html +=
@@ -374,16 +415,17 @@ exports.defineUIFunctions = function(hyper)
 				+ '</button>'
 		}
 
-		// Run button.
-		html +=
-			'<button '
-			+	'type="button" '
-			+	'class="button-run btn et-btn-green" '
-			+	'onclick="window.hyper.UI.runApp(\'__PATH3__\')">'
-			+	'Run'
-			+ '</button>'
-			+ '<h4>__NAME__</h4>'
-			+ '<p>__PATH4__</p>'
+		// Add Run button only if app has an HTML file.
+		if (appHasValidHTMLFile)
+		{
+			html +=
+				'<button '
+				+	'type="button" '
+				+	'class="button-run btn et-btn-green" '
+				+	'onclick="window.hyper.UI.runApp(\'__PATH3__\')">'
+				+	'Run'
+				+ '</button>'
+		}
 
 		if (options.deleteButton)
 		{
@@ -396,19 +438,15 @@ exports.defineUIFunctions = function(hyper)
 				+ '</button>'
 		}
 
+		// App name and path.
+		html +=
+			'<h4>__NAME__</h4>'
+			+ '<p>__PATH4__</p>'
+
 		html +=
 			'<div class="project-list-entry-path" style="display:none;">__PATH5__</div>'
 			+ '</div>'
 
-		// Get name of project, use title tag as first choise.
-		var name = hyper.UI.getProjectNameFromFile(appPath)
-		if (!name)
-		{
-			LOGGER.log('[main-window-func.js] getProjectNameFromFile failed: ' + appPath)
-
-			// Could not open the app main file, skip this app.
-			return
-		}
 
 		// Escape any backslashes in the path (needed on Windows).
 		var escapedPath = path.replace(/[\\]/g,'\\\\')
@@ -420,7 +458,7 @@ exports.defineUIFunctions = function(hyper)
 		html = html.replace('__PATH3__', escapedPath)
 		html = html.replace('__PATH4__', getShortPathFromPath(path))
 		html = html.replace('__PATH5__', path)
-		html = html.replace('__NAME__', name)
+		html = html.replace('__NAME__', appName)
 
 		// Create element.
 		var element = hyper.UI.$(html)
@@ -525,7 +563,7 @@ exports.defineUIFunctions = function(hyper)
 		var data = FILEUTIL.readFileSync(indexPath)
 		if (!data)
 		{
-			// Return null on error.
+			// Return null on error (file does not exist).
 			return null
 		}
 
