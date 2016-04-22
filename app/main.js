1d11b46a8e23ccd46afa153ba3b370e1dddb281d
Göran Krampe <goran@3dicc.com>
2016-04-22 10:55:05 +0200

    Fixed so that Workbench window can be closed and reopened (on OSX)

--- app/main.js
+++ app/main.js
@@ -410,6 +410,6 @@ app.on('activate', function () {
   // On OS X it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (main.workbenchWindow === null) {
-    app.createWorkbenchWindow();
+    main.createWorkbenchWindow();
   }
 });
