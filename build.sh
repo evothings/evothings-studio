8e450bfc781d6496721495575be7227fc18d5121
Göran Krampe <goran@3dicc.com>
2016-06-03 14:53:01 +0200

    Upgrade of electron, electron-builder, fs-extra etc and improved build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 # installed and configured with access keys.
  
 # Default if not given on command line
-VERSION=2.1.0-beta2
+VERSION=2.1.0-beta4
 
 export NAME=evothings-studio
 
@@ -12,8 +12,8 @@ function usage {
         cat <<ENDOFHELP
 Usage: $0 [version] [-ub]
 
-  version   Like "2.1.0-beta4"
-  -b        Build all platforms
+  version   Default is $VERSION
+  -b        Build
   -u        Upload to S3
   -h        Show this help.
 ENDOFHELP
@@ -21,7 +21,7 @@ ENDOFHELP
 }
 
 # Read global options and shift them away
-while getopts "uvh?" o; do
+while getopts "ubh?" o; do
 case "$o" in
    u) DOUPLOAD=true;;
    b) DOBUILD=true;;
@@ -55,8 +55,8 @@ fi
 if [ ! -z "$DOUPLOAD" ] ; then
   # Upload debs
   s3cmd put dist/*.deb s3://evothings-download/
-  s3cmd setacl --acl-public s3://evothings-download/$NAME-$VER-amd64.deb
-  s3cmd setacl --acl-public s3://evothings-download/$NAME-$VER-i386.deb
+  s3cmd setacl --acl-public s3://evothings-download/$NAME-$VER.deb
+  #s3cmd setacl --acl-public s3://evothings-download/$NAME-$VER-i386.deb
 
   # Upload (rename) Windows installer
   cp dist/win/Evothings\ Studio\ Setup\ $VER.exe /tmp/$NAME-$VER.exe
@@ -65,15 +65,15 @@ if [ ! -z "$DOUPLOAD" ] ; then
   rm /tmp/$NAME-$VER.exe
 
   # Upload (rename) OSX installer
-  cp dist/Evothings\ Studio-darwin-x64/Evothings\ Studio-$VER.dmg /tmp/$NAME-$VER.dmg
+  cp dist/osx/Evothings\ Studio-$VER.dmg /tmp/$NAME-$VER.dmg
   s3cmd put /tmp/$NAME-$VER.dmg s3://evothings-download/
   s3cmd setacl --acl-public s3://evothings-download/$NAME-$VER.dmg
   rm /tmp/$NAME-$VER.dmg
 
   # Get nice URLs out of it...
   echo PASTE INTO GITTER:
-  echo https://s3-eu-west-1.amazonaws.com/evothings-download/$NAME-$VER-amd64.deb
-  echo https://s3-eu-west-1.amazonaws.com/evothings-download/$NAME-$VER-i386.deb
+  echo https://s3-eu-west-1.amazonaws.com/evothings-download/$NAME-$VER.deb
+  #echo https://s3-eu-west-1.amazonaws.com/evothings-download/$NAME-$VER-i386.deb
   echo https://s3-eu-west-1.amazonaws.com/evothings-download/$NAME-$VER.exe
   echo https://s3-eu-west-1.amazonaws.com/evothings-download/$NAME-$VER.dmg
 fi
