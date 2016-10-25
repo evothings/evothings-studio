f0f78f4952532f86aa5595eabb6dc81b1f8e8687
Göran Krampe <goran@3dicc.com>
2016-10-25 16:43:03 +0200

    Bump to 2.2.0-beta3

--- app/main.js
+++ app/main.js
@@ -9,7 +9,7 @@ main.MAJOR = 2
 main.MINOR = 2
 main.PATCH = 0
 // Disabled for a real release:
-main.BUILD = "beta1"
+main.BUILD = "beta3"
 
 // This one is burned in by scripts
 main.TIMESTAMP = '<timestamp>'
